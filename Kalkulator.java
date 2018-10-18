d442bba67035002a774b27289cdb068f75366b06
goFrendiAsgard <gofrendiasgard@gmail.com>
2018-10-18 17:04:21 +0700

    make it simpler

--- Kalkulator.java
+++ Kalkulator.java
@@ -1,6 +1,7 @@
+import java.util.Stack;
+
 class Node {
-    float dataAngka;
-    char dataOperasi; // +, -, *, /
+    String data; // number, +, -, *, /
     Node left;
     Node right;
 }
@@ -9,45 +10,39 @@ public class Kalkulator {
     static Node root;
     public static void main(String[] args) {
         root = new Node();
-        root.dataOperasi = '*';
+        root.data = "*";
         root.left = new Node();
-        root.left.dataOperasi = '+';
+        root.left.data = "+";
         root.left.left = new Node();
-        root.left.left.dataAngka = 5;
+        root.left.left.data = "5";
         root.left.right = new Node();
-        root.left.right.dataAngka = 4;
+        root.left.right.data = "4";
         root.right = new Node();
-        root.right.dataOperasi = '-';
+        root.right.data = "-";
         root.right.left = new Node();
-        root.right.left.dataAngka = 5;
+        root.right.left.data = "5";
         root.right.right = new Node();
-        root.right.right.dataAngka = 4;
-        System.out.println(printNodeData(root.left)); // +
-        System.out.println(printNodeData(root.left.left)); // 5
+        root.right.right.data = "4";
+        System.out.println(root.left.data); // +
+        System.out.println(root.left.left.data); // 5
         System.out.println(parsePrefix(root)); // * + 5 4 - 5 4
-        System.out.println(parseSufix(root));  // 5 4 + 5 4 - *
+        System.out.println(parsePostfix(root));  // 5 4 + 5 4 - *
         System.out.println(parseInfix(root));  // 5 + 4 * 5 - 4
     }
 
-    static String printNodeData(Node node) {
-        if (node.dataOperasi == '+' || node.dataOperasi == '-' || node.dataOperasi == '*' || node.dataOperasi == '/') {
-            return "" + node.dataOperasi;
-        }
-        return Float.toString(node.dataAngka);
+    static boolean isLeaf(Node node) {
+        return node.left == null && node.right == null;
     }
 
     static String parsePrefix(Node node) {
-        return "";
-        // gimana hayo?
+        // gimana hayo
     }
 
-    static String parseSufix(Node node) {
-        return "";
-        // gimana hayo?
+    static String parsePostfix(Node node) {
+        // gimana hayo
     }
 
     static String parseInfix(Node node) {
-        return "";
-        // gimana hayo?
+        // gimana hayo
     }
 }
